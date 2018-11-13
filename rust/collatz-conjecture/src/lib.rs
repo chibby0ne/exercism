9ccf8421555624368295311b984056e3b3d8222b
Antonio Gutierrez <chibby0ne@gmail.com>
2018-11-14 00:52:44 +0100

    Add collatz-conjecture
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,6 +1,16 @@
 pub fn collatz(n: u64) -> Option<u64> {
-    unimplemented!(
-        "return Some(x) where x is the number of steps required to reach 1 starting with {}",
-        n,
-    )
+    let mut num = n;
+    let mut steps = 0;
+    while num != 0 && num != 1 {
+        match num % 2 {
+            0 => num /= 2,
+            _ => num = 3 * num + 1,
+        };
+        steps += 1;
+    }
+    match n {
+        0 => None,
+        1 => Some(0),
+        _ => Some(steps),
+    }
 }
