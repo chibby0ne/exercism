030c4f6e42e73d35692be4b114d73b42e1ebd01e
Antonio Gutierrez <chibby0ne@gmail.com>
2018-11-14 00:52:28 +0100

    Add series
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,7 +1,13 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    unimplemented!(
-        "What are the series of length {} in string {:?}",
-        len,
-        digits
-    )
+    let num_of_digits = digits.len();
+    let mut temp: Vec<String> = Vec::new();
+    if num_of_digits < len {
+        return temp;
+    } else if len == 0 {
+        return vec!["".to_string(); num_of_digits + 1];
+    }
+    for i in 0..=num_of_digits - len {
+        temp.push(digits.get(i..i + len).unwrap().to_string());
+    }
+    return temp;
 }
