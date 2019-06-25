a99cc1dd8c8eb8b6cf560fc95e5d001fbd680684
Antonio Gutierrez <chibby0ne@gmail.com>
2019-06-25 23:33:28 +0200

    rust/pascals-triangle: improve solution a bit more
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -12,15 +12,11 @@ impl PascalsTriangle {
         for i in 0..self.count {
             let mut inner_vec: Vec<u32> = Vec::with_capacity(i as usize + 1);
             for j in 0..=i {
-                if j == 0 || j == inner_vec.capacity() as u32 {
-                    inner_vec.push(1)
+                if j == 0 || j as usize == inner_vec.capacity() - 1 {
+                    inner_vec.push(1);
                 } else {
                     let prev_vec = vec.get(i as usize - 1).unwrap();
-                    if j as usize == inner_vec.capacity() - 1 {
-                        inner_vec.push(prev_vec[j as usize - 1]);
-                    } else {
-                        inner_vec.push(prev_vec[j as usize - 1] + prev_vec[j as usize]);
-                    }
+                    inner_vec.push(prev_vec[j as usize - 1] + prev_vec[j as usize]);
                 }
             }
             vec.push(inner_vec);
