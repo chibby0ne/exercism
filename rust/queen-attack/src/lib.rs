911ceedbda429222ab200b520fc26a126d6e499e
Antonio Gutierrez <chibby0ne@gmail.com>
2019-09-15 20:18:54 +0200

    rust/queen-attack: refactor in_same_diagonal
    Checking if two positions are in the same diagonal can be easily done by
    comparing the absolute value of the difference between one position and
    another of one coordinate with the absolute value of the difference
    between the same two points of the other coordinate, i.e: the absolute
    difference between the ranks of one point and the other point should be
    equals to the absolute difference between the files of those two points,
    if they are diagonal to each other in the board.
    
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug)]
 pub struct ChessPosition {
     rank: i32,
     file: i32,
@@ -12,10 +12,7 @@ pub struct Queen {
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
         match (rank, file) {
-            (0..=7, 0..=7) => Some(ChessPosition {
-                rank,
-                file,
-            }),
+            (0..=7, 0..=7) => Some(ChessPosition { rank, file }),
             _ => None,
         }
     }
@@ -29,55 +26,7 @@ impl ChessPosition {
     }
 
     pub fn in_same_diagonal(&self, other: &ChessPosition) -> bool {
-        let mut rank = self.rank;
-        let mut file = self.file;
-
-        // going south east
-        while rank < 8 && file < 8 {
-            rank += 1;
-            file += 1;
-            let temp_pos = &ChessPosition { rank, file };
-            if temp_pos == other {
-                return true;
-            }
-        }
-
-        // going north east
-        rank = self.rank;
-        file = self.file;
-        while rank >= 0 && file < 8 {
-            rank -= 1;
-            file += 1;
-            let temp_pos = &ChessPosition { rank, file };
-            if temp_pos == other {
-                return true;
-            }
-        }
-
-        // going north west
-        rank = self.rank;
-        file = self.file;
-        while rank >= 0 && file >= 0 {
-            rank -= 1;
-            file -= 1;
-            let temp_pos = &ChessPosition { rank, file };
-            if temp_pos == other {
-                return true;
-            }
-        }
-
-        // going south west
-        rank = self.rank;
-        file = self.file;
-        while rank < 8 && file >= 0 {
-            rank += 1;
-            file -= 1;
-            let temp_pos = &ChessPosition { rank, file };
-            if temp_pos == other {
-                return true;
-            }
-        }
-        false
+        (self.file - other.file).abs() == (self.rank - other.rank).abs()
     }
 }
 
