cf1bbe3c9f2a324fab6933ea7e53462c924bc69a
Antonio Gutierrez <chibby0ne@gmail.com>
2020-01-04 03:09:10 +0100

    rust: tournament (use partialOrd, Ord, Eq, partialEq) to sort
    Using the points and then tie break using the name of the teams.
    
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-const HEADER: &str = "Team                           | MP |  W |  D |  L |  P\n";
-
 const TEAM_NAMES: [&str; 4] = [
     "Devastating Donkeys",
     "Allegoric Alaskans",
@@ -17,6 +15,7 @@ enum Outcome {
 
 #[derive(Default, Debug)]
 struct Team {
+    name: String,
     matches: u16,
     wins: u16,
     draws: u16,
@@ -25,9 +24,35 @@ struct Team {
 }
 
 impl Team {
-    pub fn new() -> Self {
+    pub fn new(name: String) -> Self {
         Team {
+            name,
             ..Default::default()
+        }
+    }
+}
+
+impl PartialEq for Team {
+    fn eq(&self, rhs: &Team) -> bool {
+        self.name == rhs.name
+    }
+}
+
+impl PartialOrd for Team {
+    fn partial_cmp(&self, rhs: &Team) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(rhs))
+    }
+}
+
+impl Eq for Team {}
+
+impl Ord for Team {
+    fn cmp(&self, other: &Team) -> std::cmp::Ordering {
+        if self.points == other.points {
+            self.name.cmp(&other.name)
+        } else {
+            // since it is decreasing order
+            other.points.cmp(&self.points)
         }
     }
 }
@@ -50,18 +75,15 @@ fn update_score(all_teams: &mut HashMap<String, Team>, team: &str, outcome: Outc
 }
 
 pub fn tally(match_results: &str) -> String {
-    let mut result = String::from(HEADER);
-
     let mut all_teams = TEAM_NAMES
         .iter()
-        .map(|v| (v.to_string(), Team::new()))
+        .map(|v| (v.to_string(), Team::new(v.to_string())))
         .collect::<HashMap<String, Team>>();
-
     for line in match_results.split('\n') {
         let mut iter = line.split(';');
         let first_team = iter.next().unwrap();
         if first_team == "" {
-            return result.trim_end_matches('\n').to_string();
+            break;
         }
         let second_team = iter.next().unwrap();
         if let Some(outcome) = iter.next() {
@@ -82,15 +104,17 @@ pub fn tally(match_results: &str) -> String {
             }
         }
     }
-    let mut count_vec: Vec<(&String, &Team)> = all_teams.iter().collect();
-    count_vec.sort_unstable_by_key(|&x| x.0); // ties are settled by teams alphabetical name
-    count_vec.sort_by(|&a, &b| b.1.points.cmp(&a.1.points)); // sorted descending order by points
-
-    for (team_name, team) in count_vec.iter().filter(|x| x.1.matches > 0) {
+    let mut result = format!("{:30} | MP |  W |  D |  L |  P\n", "Team");
+    if all_teams.is_empty() {
+        return result.trim_end_matches('\n').to_string();
+    }
+    let mut count_vec: Vec<&Team> = all_teams.iter().map(|(_, y)| y).collect();
+    count_vec.sort();
+    for team in count_vec.iter().filter(|x| x.matches > 0) {
         result.push_str(
             format!(
                 "{:30} | {:2} | {:2} | {:2} | {:2} | {:2}\n",
-                team_name, team.matches, team.wins, team.draws, team.losses, team.points
+                team.name, team.matches, team.wins, team.draws, team.losses, team.points
             )
             .as_str(),
         )
