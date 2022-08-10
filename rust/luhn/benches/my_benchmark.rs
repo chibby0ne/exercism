e862a0981257d1aed016d62c069703ec39317dc8
Antonio Gutierrez <chibby0ne@gmail.com>
2022-08-10 10:51:42 +0200

    rust: improve luhn using benchmarks and criterion
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

---
+++ rust/luhn/benches/my_benchmark.rs
@@ -0,0 +1,13 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use luhn::{is_valid, is_valid2};
+
+
+
+fn bench_is_valid(c: &mut Criterion) {
+    c.bench_function("is_valid", |b| b.iter(|| is_valid(black_box("234 567 891 234"))));
+    c.bench_function("is_valid2", |b| b.iter(|| is_valid2(black_box("234 567 891 234"))));
+}
+
+criterion_group!(benches, bench_is_valid);
+criterion_main!(benches);
+
--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,5 +1,5 @@
 /// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool {
+pub fn is_valid2(code: &str) -> bool {
     let iter = code.chars().filter_map(|x| x.to_digit(10));
     // If all chars are not digits or whitespaces the number is invalid according to the Luhn
     // formula
@@ -24,4 +24,31 @@ pub fn is_valid(code: &str) -> bool {
             .chain(iter.clone().rev().step_by(2));
         res.sum::<u32>() % 10 == 0
     }
+}
+
+
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    let mut sum = 0;
+    let mut numbers = 0;
+    for c in code.bytes().rev() {
+        match c {
+            b'0'..=b'9' => {
+                let mut val = c - b'0';
+                if val != 0 && numbers % 2 == 1 {
+                    val *= 2;
+                    if val > 9 {
+                        val -= 9;
+                    }
+                }
+                sum += val;
+                numbers += 1;
+            }
+            b' ' => {}
+            _ => {
+                return false;
+            }
+        }
+    }
+    numbers > 1 && sum % 10 == 0
 }
