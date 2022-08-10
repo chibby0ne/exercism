1cb5b62b58064a2720d263440785a879bd97ea62
Antonio Gutierrez <chibby0ne@gmail.com>
2022-08-10 19:15:01 +0200

    rust: Add 3rd solution to Luhn
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/luhn/benches/my_benchmark.rs
+++ rust/luhn/benches/my_benchmark.rs
@@ -1,11 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use luhn::{is_valid, is_valid2};
+use luhn::{is_valid, is_valid2, is_valid3};
 
 
 
 fn bench_is_valid(c: &mut Criterion) {
     c.bench_function("is_valid", |b| b.iter(|| is_valid(black_box("234 567 891 234"))));
     c.bench_function("is_valid2", |b| b.iter(|| is_valid2(black_box("234 567 891 234"))));
+    c.bench_function("is_valid3", |b| b.iter(|| is_valid2(black_box("234 567 891 234"))));
 }
 
 criterion_group!(benches, bench_is_valid);
--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,10 +1,37 @@
+/// time: [22.240 ns 22.821 ns 23.608 ns]
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    let (mut sum, mut numbers): (u32, u32) = (0, 0);
+    for c in code.bytes().rev() {
+        match c {
+            b'0'..=b'9' => {
+                let mut val = (c - b'0') as u32;
+                if val != 0 && numbers % 2 == 1 {
+                    val *= 2;
+                    if val > 9 {
+                        val -= 9;
+                    }
+                }
+                sum += val;
+                numbers += 1;
+            }
+            b' ' => {}
+            _ => {
+                return false;
+            }
+        }
+    }
+    numbers > 1 && sum % 10 == 0
+}
+
+/// time: [108.66 ns 109.53 ns 110.41 ns]
 /// Check a Luhn checksum.
 pub fn is_valid2(code: &str) -> bool {
     let iter = code.chars().filter_map(|x| x.to_digit(10));
     // If all chars are not digits or whitespaces the number is invalid according to the Luhn
     // formula
     // Likewise if has less than 2 digits (ignoring whitespaces) it is invalid
-    if !code.chars().all(|x| x.is_digit(10) || x.is_whitespace()) || iter.clone().count() < 2 {
+    if !code.chars().all(|x| x.is_ascii_digit() || x.is_whitespace()) || iter.clone().count() < 2 {
         false
     } else {
         // It is valid if the sum of the adjusted double of every second digit from the right, with all the other digits is a multiple of 10
@@ -26,16 +53,14 @@ pub fn is_valid2(code: &str) -> bool {
     }
 }
 
-
+/// time:   [109.95 ns 111.70 ns 113.62 ns]
 /// Check a Luhn checksum.
-pub fn is_valid(code: &str) -> bool {
-    let mut sum = 0;
-    let mut numbers = 0;
-    for c in code.bytes().rev() {
-        match c {
-            b'0'..=b'9' => {
-                let mut val = c - b'0';
-                if val != 0 && numbers % 2 == 1 {
+pub fn is_valid3(code: &str) -> bool {
+    let (mut sum, mut numbers): (u32, u32) = (0, 0);
+    for c in code.chars().rev() {
+        match c.to_digit(10) {
+            Some(mut val) => {
+                if val != 0 && numbers % 2 == 1{
                     val *= 2;
                     if val > 9 {
                         val -= 9;
@@ -44,9 +69,10 @@ pub fn is_valid(code: &str) -> bool {
                 sum += val;
                 numbers += 1;
             }
-            b' ' => {}
-            _ => {
-                return false;
+            None => {
+                if c != ' ' {
+                    return false;
+                }
             }
         }
     }
