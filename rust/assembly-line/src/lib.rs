9bc28be9bdc759ff658b223940f9fe6868e131dc
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-15 00:33:34 +0100

    rust: refactor assembly-line
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -2,22 +2,18 @@
 // to enable stricter warnings.
 #![allow(unused)]
 
-fn success_rate(speed: u8) -> f64 {
-    match speed {
-        0 => 0.0,
-        1..=4 => 1.0,
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    let success_rate = match speed {
+        0..=4 => 1.0,
         5..=8 => 0.9,
         9..=10 => 0.77,
         _ => {
-            panic!("{}", format!("invalid speed: {}", speed));
+            panic!("invalid speed: {}", speed)
         }
-    }
-}
-
-pub fn production_rate_per_hour(speed: u8) -> f64 {
-    success_rate(speed) * speed as f64 * 221.0
+    };
+    success_rate * speed as f64 * 221.0
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    (production_rate_per_hour(speed) / 60.0) as u32
+    production_rate_per_hour(speed) as u32 / 60
 }
