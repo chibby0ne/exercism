5a8835cdb8eda7913ffefefcab7ff46a208209fa
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-14 04:12:59 +0100

    Add Luhn-from
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -1,4 +1,3 @@
-use std::any::{TypeId, Any};
 use std::fmt::Display;
 
 pub struct Luhn {
@@ -7,27 +6,51 @@ pub struct Luhn {
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
+        let length = self.value.len();
 
+        // Length <= 1 are invalid
+        if length <= 1 {
+            return false;
+        }
+
+        // Allocate a vector with as much capacity as there are chars
+        let mut array_digits: Vec<u64> = Vec::with_capacity(length);
+
+        // Starting from the right double every second number and if the result bigger than 9, substract
+        // 9, then add all the digits including the ones not modified and if the sum is evenly
+        // divisible by 10 it is Luhn valid
+        for (i, c) in self.value.chars().rev().enumerate() {
+            // Spaces are allowed in input but they should be stripped (or ignored)
+            if c.is_whitespace() {
+                continue;
+            }
+            // Any other non-digit char is disallowed
+            if !c.is_ascii_digit() {
+                return false;
+            }
+            // Convert the char to digit
+            let mut num = c.to_digit(10).expect("Expected a number");
+            // If it's every second we double it and if it's bigger we substract by 9
+            if i % 2 == 1 {
+                num *= 2;
+                if num > 9 {
+                    num -= 9;
+                }
+            }
+            array_digits.push(num as u64);
+        }
+        array_digits.iter().sum::<u64>() % 10 == 0
     }
 }
 
-const STRING: TypeId  = TypeId::of::<String>();
-const STR: TypeId  = TypeId::of::<&str>();
-const USIZE: TypeId = TypeId::of::<usize>();
-const U8: TypeId = TypeId::of::<u8>();
-const U16: TypeId = TypeId::of::<u16>();
-const U32: TypeId = TypeId::of::<u32>();
-const U64: TypeId = TypeId::of::<u64>();
-
-impl<'a, T: Any + Display> From<&'a T> for Luhn {
-    fn from(input: &'a T) -> Self {
-        let val = match TypeId::of::<T>() {
-            STRING | STR => input.to_string(),
-            _ => String::from(""),
-
-        };
+impl<T: Display> From<T> for Luhn {
+    fn from(input: T) -> Self {
+        // Convert to String and retain all the non-whitespace characters of the String
+        // representation
+        let mut s: String = input.to_string();
+        s.retain(|c| !c.is_whitespace());
         Self {
-            value: val,
+            value: s,
         }
     }
 }
