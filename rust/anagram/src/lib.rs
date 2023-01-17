59db46bd867bd1b51960ce9a25072087c56efe9e
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-18 00:08:42 +0100

    rust: refactor anagram with mentoring
    Use a vector instead of hashmap. Hashmaps have average O(1) time for
    get/insert but this constant is big due to calculating the hash. Also
    are bigger space wise.
    
    Using a vector requires O(N) time but uses less time and perhaps the
    difference in time for smaller words is negligible.
    
    In addition we now use sort of both the chars of the word and the
    possible_anagram to compare them, this requires O(N log N) but is the
    fastest approach to compare the equality of the strings using vectors.
    
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,52 +1,32 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-// Time complexity: O(word_length + possible_length)
+// Time complexity: O(N log N) -> sorts
 // Space complexity: O(word_length + possible_length)
-fn is_anagram(word: &str, possible: &str) -> bool {
-    if word.len() != possible.len() {
-        return false;
+fn is_anagram<'a>(word: &'a str, possible: &'a str) -> Option<&'a str> {
+    let word_lowercase = word.to_lowercase();
+    let possible_lowercase = possible.to_lowercase();
+    if word_lowercase == possible_lowercase || word_lowercase.len() != possible_lowercase.len() {
+        return None;
     }
-    let mut map_word: HashMap<char, u32> = HashMap::new();
-    let mut map_possible: HashMap<char, u32> = HashMap::new();
-    for letter in possible.chars() {
-        map_possible
-            .entry(letter)
-            .and_modify(|count| *count += 1)
-            .or_insert(1);
+
+    let mut vec_word: Vec<char> = word_lowercase.chars().collect();
+    let mut vec_possible: Vec<char> = possible_lowercase.chars().collect();
+
+    vec_word.sort();
+    vec_possible.sort();
+
+    if vec_word == vec_possible {
+        Some(possible)
+    } else {
+        None
     }
-    for letter in word.chars() {
-        map_word
-            .entry(letter)
-            .and_modify(|count| *count += 1)
-            .or_insert(1);
-    }
-    for (letter, count) in map_word {
-        match map_possible.get(&letter) {
-            Some(&count_possible) => {
-                if count_possible != count {
-                    return false;
-                }
-            }
-            None => {
-                return false;
-            }
-        }
-    }
-    true
 }
 
 // Time complexity: O(possible_anagrams_length * O(word_length + MAX(possible_anagrams_length))
 // Space complexity:  O(word_length + possible_length)
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut result: HashSet<&str> = HashSet::new();
-    let word_lowercase = word.to_lowercase();
-    for possible in possible_anagrams {
-        let possible_lowercase = possible.to_lowercase();
-        if !possible_lowercase.eq(word_lowercase.as_str())
-            && is_anagram(word_lowercase.as_str(), possible_lowercase.as_str())
-        {
-            result.insert(possible);
-        }
-    }
-    result
+pub fn anagrams_for<'a>(word: &'a str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    possible_anagrams
+        .iter()
+        .filter_map(|&s| is_anagram(word, s))
+        .collect()
 }
