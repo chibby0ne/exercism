891ddf5388966f6fa7ef6240b6a3d5da087103ce
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-18 23:51:12 +0100

    rust: refactor evens function to use step_by(2)
    Instead of using `enumerate()` chained with then a `filter_map` for
    those whose `index % 2 == 0`, an even cleaner more elegant way is to use
    `step_by(2)`
    
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -7,8 +7,7 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate()
-        .filter_map(|(i, x)| if i % 2 == 0 { Some(x) } else { None })
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
