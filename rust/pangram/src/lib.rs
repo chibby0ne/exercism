facdea353dfe73ace74c377fef02c475593dcdda
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-22 04:41:16 +0100

    rust: refactor pangram to use no for loops
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,19 +1,10 @@
 use std::collections::HashSet;
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let set: HashSet<u8> = sentence
+    let set: HashSet<_> = sentence
         .to_ascii_uppercase()
         .chars()
-        .filter(|x| x.is_ascii())
-        .fold(HashSet::new(), |mut acc, c| {
-            acc.insert(c as u8);
-            acc
-        });
-
-    for c in b'A'..=b'Z' {
-        if !set.contains(&c) {
-            return false;
-        }
-    }
-    true
+        .filter(|x| x.is_ascii_uppercase())
+        .collect();
+    set.len() == 26
 }
