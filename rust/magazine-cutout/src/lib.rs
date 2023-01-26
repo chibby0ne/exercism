8940345de0777c54b544332ceb5ddc18bcb6ef84
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-26 23:35:05 +0100

    rust: refactor more functional magazine-cutout
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,22 +5,17 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    let mut map: HashMap<&str, u64> = HashMap::new();
-    for word in magazine {
-        *map.entry(word).or_insert(0) += 1;
-    }
+    let mut map = magazine.iter().fold(HashMap::new(), |mut m, word| {
+        *m.entry(word).or_insert(0) += 1;
+        m
+    });
 
     for word in note {
-        match map.get_mut(word) {
-            Some(mut v) => {
-                if *v > 0 {
-                    *v -= 1
-                } else {
-                    return false;
-                }
-            }
-            None => return false,
+        let entry = map.entry(word).or_insert(0);
+        if *entry == 0 {
+            return false;
         }
+        *entry += 1;
     }
     true
 }
