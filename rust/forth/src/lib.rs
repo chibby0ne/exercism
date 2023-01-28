f14c6aae454effb9bbd5a3a11989cc5390218c95
Antonio Gutierrez <chibby0ne@gmail.com>
2023-01-28 04:51:15 +0100

    rust: refactor forth
    Signed-off-by: Antonio Gutierrez <chibby0ne@gmail.com>

--- rust/forth/src/lib.rs
+++ rust/forth/src/lib.rs
@@ -79,6 +79,12 @@ impl Forth {
         if self.definitions.get(name).is_some() {
             let defs = self.definitions.clone();
             for (k, v) in defs {
+                // Cannot use replace on v because it might be a substring of something other value e.g:
+                // ": foo 10 ;"
+                // ": bar foobar ;"
+                // ": foo 15 ;"
+                // In this case if we used replace we would replace bar definition to:
+                // ": bar 15bar ;"
                 let new_v: String = v
                     .split_whitespace()
                     .map(|word| {
@@ -101,8 +107,9 @@ impl Forth {
         let mut definition = String::new();
         let mut token = tokens.next();
         while token.is_some() && token.unwrap() != ";" {
-            // If the definition is using the same name, then theres a previous definition with
-            // this name and we should expand it
+            // If the definition is using the same name, then there's a previous definition with
+            // this name and we should expand it (only this case and not other otherwise we are
+            // applying eager expansion which is exploitable see tests/alloc-attack.rs)
             if name == token.unwrap() {
                 definition.push_str(self.definitions.get(name).unwrap());
             } else {
@@ -111,9 +118,11 @@ impl Forth {
             definition.push(' ');
             token = tokens.next();
         }
+
+        // Trim the extra space at the end of definition just because it looks better when debugging the definitions hashmap
         definition = definition.trim_end().to_string();
 
-        // Check that it ends with a ;
+        // Check that it ends with a ;, if not then return error as per README
         if token.is_none() {
             return Err(Error::InvalidWord);
         }
@@ -122,96 +131,59 @@ impl Forth {
         Ok(())
     }
 
+    fn handle_definition(&mut self, token: &str) -> Result {
+        // Need to clone since we use inmutable borrow of self (when using the get on definitions)
+        // while at the same time having a self mutable borrow of definition for eval
+        let v = self.definitions.clone();
+        let def = v.get(token).unwrap();
+        self.eval(def)
+    }
+
     pub fn eval(&mut self, input: &str) -> Result {
         let tokens: Vec<String> = input.split_whitespace().map(|t| t.to_uppercase()).collect();
         let mut iter = tokens.iter();
         while let Some(token) = iter.next() {
-            match token.as_str() {
-                // Handle arithmetic
-                "+" | "-" | "*" | "/" => {
-                    if self.definitions.get(token).is_some() {
-                        let v = self.definitions.clone();
-                        let def = v.get(token).unwrap();
-                        self.eval(def)?;
-                    } else {
-                        self.do_arithmetic(token)?
-                    }
-                }
-                // Duplicates the top item
-                "DUP" => {
-                    if self.definitions.get(token).is_some() {
-                        let v = self.definitions.clone();
-                        let def = v.get(token).unwrap();
-                        self.eval(def)?;
-                    } else {
+            let token_str = token.as_str();
+            match self.definitions.get(token_str) {
+                // Try to handle definitions first since they could overload pretty much anything
+                Some(_) => self.handle_definition(token_str)?,
+                // If there are no definitions with that name then use try to match to the arithmetic operators, functions and numbers
+                None => match token.as_str() {
+                    "+" | "-" | "*" | "/" => self.do_arithmetic(token)?,
+                    // Duplicates the top item
+                    "DUP" => {
                         let val = self.peek_stack()?;
                         self.stack.push(val);
                     }
-                }
-                // Discards the top stack item
-                "DROP" => {
-                    if self.definitions.get(token).is_some() {
-                        let v = self.definitions.clone();
-                        let def = v.get(token).unwrap();
-                        self.eval(def)?;
-                    } else {
+                    // Discards the top stack item
+                    "DROP" => {
                         self.pop_stack()?;
                     }
-                }
-                // Swaps the two top stack items
-                "SWAP" => {
-                    if self.definitions.get(token).is_some() {
-                        let v = self.definitions.clone();
-                        let def = v.get(token).unwrap();
-                        self.eval(def)?;
-                    } else {
+                    // Swaps the two top stack items
+                    "SWAP" => {
                         let top = self.pop_stack()?;
                         let second_top = self.pop_stack()?;
                         self.stack.push(top);
                         self.stack.push(second_top);
                     }
-                }
 
-                // Copies the second item to top
-                "OVER" => {
-                    if self.definitions.get(token).is_some() {
-                        let v = self.definitions.clone();
-                        let def = v.get(token).unwrap();
-                        self.eval(def)?;
-                    } else {
-                        match self.stack.iter().rev().nth(1) {
-                            Some(&second_top) => self.stack.push(second_top),
-                            None => return Err(Error::StackUnderflow),
-                        }
+                    // Copies the second item to top
+                    "OVER" => match self.stack.iter().rev().nth(1) {
+                        Some(&second_top) => self.stack.push(second_top),
+                        None => return Err(Error::StackUnderflow),
+                    },
+                    // Define name -> definitions
+                    ":" => {
+                        self.create_definition(&mut iter)?;
                     }
-                }
-                // Define definitions
-                ":" => {
-                    self.create_definition(&mut iter)?;
-                }
-                _ => {
-                    if token.chars().all(|c| c.is_ascii_digit()) {
-                        self.stack.push(token.parse().unwrap());
-                    } else if self.definitions.get(token).is_none() {
-                        return Err(Error::UnknownWord);
-                    } else {
-                        let v = self.definitions.clone();
-                        let def = v.get(token).unwrap();
-                        self.eval(def)?;
+                    // Push numbers to stack or throw error for UnknownWord since it's none of the above
+                    _ => {
+                        match token.parse() {
+                            Ok(val) => self.stack.push(val),
+                            Err(_) => return Err(Error::UnknownWord),
+                        };
                     }
-
-                    // Do the definition
-                    // self.eval(
-
-                    // match token.parse() {
-                    //     Ok(val) => {
-                    //         self.stack.push(val);
-                    //     }
-                    //     Err(_) => {
-                    //         return Err(Error::UnknownWord);
-                    //     }
-                    // };
-                }
+                },
             };
         }
         Ok(())
